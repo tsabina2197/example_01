265c89c21d17e1d106ed96ec5e614e8939dc5c88
tsabina2197 <tsabina2197@gmail.com>
2025-12-19 16:34:59 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+roy = int(input("Enter month: "))
+
+if roy == 12 or roy == 1 or roy == 2:
+    print("Winter")
+elif roy == 3 or roy == 4 or roy == 5:
+    print("Spring")
+elif roy == 6 or roy == 7 or roy == 8:
+    print("Summer")
+elif roy == 9 or roy == 10 or roy == 11:
+    print("Autumn")
+else:
+    print("Error")
